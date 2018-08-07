43999f827b2db90552d2f62b4b6e351219c502f8
XI328883 <p000rbertoni@prservicos.com.br>
2018-08-06 23:41:02 -0300

    Pegando a lista

--- leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuarioWSTest.java
+++ leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuarioWSTest.java
@@ -2,6 +2,8 @@ package br.com.caelum.leilao.teste;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.jayway.restassured.path.xml.XmlPath;
@@ -22,14 +24,15 @@ public class UsuarioWSTest {
 						andReturn().
 						xmlPath();
 		
-		Usuario usuario1 = path.getObject("list.usuario[0]", Usuario.class);
-		Usuario usuario2 = path.getObject("list.usuario[1]", Usuario.class);
+		/*Usuario usuario1 = path.getObject("list.usuario[0]", Usuario.class);
+		Usuario usuario2 = path.getObject("list.usuario[1]", Usuario.class);*/
+		List<Usuario> usuarios = path.getList("list.usuario", Usuario.class);
 		
 		Usuario esperado1 = new Usuario(1L, "Mauricio Aniche","mauricio.aniche@caelum.com.br");
 		Usuario esperado2 = new Usuario(2L, "Guilherme Silveira","guilherme.silveira@caelum.com.br");
 		
-		assertEquals(esperado1, usuario1);
-		assertEquals(esperado2, usuario2);
+		assertEquals(esperado1, usuarios.get(0));
+		assertEquals(esperado2, usuarios.get(1));
 	}
 
 }
